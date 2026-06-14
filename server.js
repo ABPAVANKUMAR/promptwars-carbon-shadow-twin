693b9059f5afc38465f6999aa384d9edef728cb3
ABPAVANKUMAR <abpavankumar4052@gmail.com>
2026-06-14 22:19:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const MIME = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.json': 'application/json',
+  '.ico': 'image/x-icon'
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': MIME[ext] || 'text/plain' });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Carbon Shadow Twin running at http://localhost:${PORT}`);
+});
 
